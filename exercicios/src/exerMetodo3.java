0c0f64cb7ce5a5d9e96d9a6ac323f4b658cd9afa
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-03-03 21:00:48 -0300

    adiciona
    atividades feitas com professor

--- exercicios/src/exerMetodo3.java
+++ exercicios/src/exerMetodo3.java
@@ -7,20 +7,19 @@ public class exerMetodo3
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("Insira um número: ");
-		int numInicial = input.nextInt();
 		
-		exibirNumeros(numInicial);
+		System.out.println("Insira um número: ");
+		int numero = input.nextInt();
+		
+		exibirNumeros(numero);
 	}
 	
-	public static void exibirNumeros(int numero)
+	public static void exibirNumeros(int numInicial)
 	{
-	    System.out.println("Número inicial: " + numero);
-	    System.out.println("Os 20 números seguintes são:");
-
-	    for (int contador = 1; contador <= 20; contador++) {
-	        System.out.print((numero + contador) + " ");
+	    for (int i = numInicial; i <= numInicial + 20; i++)
+	    {
+	    	System.out.print(i +" ");
 	    }
 	}
-	  
+	
 }
---
+++ exercicios/src/exerMetodo4.java
@@ -0,0 +1,62 @@
+
+public class exerMetodo4 {
+
+	public static void main(String[] args) {
+
+		int vetor[] = { 8, 14, 2, 16, -5, 0 };
+
+		System.out.print("O maior valor é: ");
+		obterMaiorValor(vetor);
+		System.out.print("O menor valor é: ");
+		obterMenorValor(vetor);
+		System.out.print("A média é: ");
+		obterMedia(vetor);
+	}
+
+	public static void obterMaiorValor(int vetor[]) {
+
+		int maiorValor = Integer.MIN_VALUE;
+
+
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] > maiorValor) {
+				maiorValor = vetor[i];
+			}
+		}
+
+		System.out.println(maiorValor);
+
+	}
+
+	public static void obterMenorValor(int vetor[]) {
+
+		int menorValor = Integer.MAX_VALUE;
+
+
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] < menorValor) {
+				menorValor = vetor[i];
+			}
+		}
+
+		System.out.println(menorValor);
+
+	}
+
+	public static void obterMedia(int vetor[]) {
+
+		double soma = 0;
+
+		for (int i = 0; i < vetor.length; i++) {
+			soma += vetor[i];
+		}
+
+		double media = soma / vetor.length;
+
+		System.out.println(media);
+
+	}
+
+}
+
+
