116d44fb2889ea666bb141cbb8f3671c9072aa1f
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-03-17 21:54:29 -0300

    adiciona
    Atividade seis mercado

--- exercicios/src/mercadoExerSeis/mercado.java
+++ exercicios/src/mercadoExerSeis/mercado.java
@@ -1,10 +1,19 @@
 package mercadoExerSeis;
 
 public class mercado {
+	
 	String nome;
 	int macaAno;
 	double precoMaca;
 	int laranjaAno;
 	double precoLaranja;
-	}
+	double total;
 
+
+public double total(){
+	
+	return total = (macaAno*precoMaca) + (laranjaAno*precoLaranja);
+
+	
+}
+}
