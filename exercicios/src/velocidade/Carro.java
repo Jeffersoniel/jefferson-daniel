fa01cded6f5339cf6a6bd42031a04d8f12f9d585
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-04-07 21:24:54 -0300

    adiciona
    adiciona e melhora

---
+++ exercicios/src/velocidade/Carro.java
@@ -0,0 +1,35 @@
+package velocidade;
+
+public class Carro {
+
+	private int velocidade;
+
+	public Carro(int velocidade) {
+		setVelocidade(velocidade);
+		}
+	
+	public int getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(int velocidade) {
+		if (velocidade < 0) {
+			throw new IllegalArgumentException("Velocidade invalida");
+		}
+		this.velocidade = velocidade;
+	}
+
+	public void acelerar(int valor) {
+		if (valor < 0 || valor >= 20) {
+			throw new IllegalArgumentException("Valor inválido para acelerar (0 a 19)");
+		}
+		setVelocidade(velocidade + valor);
+	}
+
+	public void reduzir(int valor) {
+		if (valor <= 0 || valor > 30) {
+			throw new IllegalArgumentException("Valor inválido para reduzir (0 a 29)");
+		}
+		setVelocidade(velocidade - valor);
+	}
+}
---
+++ exercicios/src/velocidade/Ex9.java
@@ -0,0 +1,19 @@
+package velocidade;
+
+public class Ex9{
+	public static void main(String[] args) {
+        Carro carro = new Carro(30);
+
+        try {
+            carro.acelerar(10);
+            System.out.println("Velocidade: " + carro.getVelocidade());
+
+            carro.reduzir(5);
+            System.out.println("Velocidade: " + carro.getVelocidade());
+
+            carro.acelerar(25);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
