59ce0bef23eb4b4b6b44c5929410c5719059298e
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-04-23 19:34:19 -0300

    adiciona

--- retangulo/src/Retangulo/ColetaneoDeRetangulo.java
+++ retangulo/src/Retangulo/ColetaneoDeRetangulo.java
@@ -1,24 +1,28 @@
 package Retangulo;
 
-public class ColetaneoDeRetagulo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ColetaneoDeRetangulo {
+
+	
 	private List<Retangulo> listaRetangulos;
 	
 	public ColetaneoDeRetangulo() {
-		listaRetangulos = new Arraylist<>();
+		listaRetangulos = new ArrayList<>();
 	}
 	
 	public void adicionarRetangulo(Retangulo r) {
 		listaRetangulos.add(r);
 	}
 	
-	public list<Retangulo> obterLista(){
+	public List<Retangulo> obterLista(){
 		return listaRetangulos;
 	}
 	
 	public Retangulo obterRetanguloMaiorPerimetro() {
 		double maiorPerimetro = Double.MIN_VALUE;
-		
+		Retangulo retMaiorPerimetro = null;
 		for(Retangulo r : listaRetangulos) {
 			if(r.obterPerimetro()> maiorPerimetro) {
 				maiorPerimetro = r.obterPerimetro();
@@ -30,7 +34,7 @@ public class ColetaneoDeRetagulo {
 	
 	public Retangulo obterRetanguloMaiorArea() {
 		double  maiorArea = Double.MIN_VALUE;
-		
+		Retangulo retMaiorArea = null;
 		for(Retangulo r : listaRetangulos) {
 			if (r.obterArea()>maiorArea) {
 				maiorArea = r.obterArea();
--- retangulo/src/Retangulo/Retangulo.java
+++ retangulo/src/Retangulo/Retangulo.java
@@ -2,9 +2,41 @@ package Retangulo;
 
 public class Retangulo {
 
+	private double largura;
 	private double altura;
-	private double altura;
 	
+	public Retangulo(double altura, double largura) {
+		setAltura(altura);
+		setLargura(largura);
+	}
 	
+	public double obterArea() {
+		return largura * altura;
+	}
 	
+	public double obterPerimetro() {
+		return (largura + altura) * 2;
+	}
+	
+	public double getLargura() {
+		return largura;
+	}
+	public void setLargura(double largura) {
+		if (largura <= 0) {
+			throw new IllegalArgumentException("Altura inválida");
+		}
+		this.largura = largura;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		if (altura <= 0) {
+			throw new IllegalArgumentException("Largura inválida");}
+		this.altura = altura;
+	}
+	@Override
+	public String toString() {
+		return "Retangulo [largura=" + largura + ", altura=" + altura + "]";
+}
 }
