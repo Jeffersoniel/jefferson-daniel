c0bc8185d403fdb15b402c6f4c7d8d7ea211113a
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-04-23 21:36:07 -0300

    adiciona

--- Flor/src/Flor.java
+++ Flor/src/Flor.java
@@ -1,4 +1,48 @@
 
 public class Flor {
 
+	private String flor;
+	private double valor;
+	private String nome;
+	
+	public Flor(String flor, double valor,String nome) {
+		setFlor(flor);
+		setNome(nome);
+		setValor(valor);
+	}
+
+	public String getFlor() {
+		return flor;
+	}
+
+	public void setFlor(String flor) {
+		if (flor == null || flor.isBlank()){
+			throw new IllegalArgumentException("Flor inválida");
+	}
+		this.flor = flor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		if(valor <=0) {
+			throw new IllegalArgumentException("Valor inválido");
+		}
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome == null || nome.isBlank()) {
+			throw new IllegalArgumentException("Nome inválido");
+		}
+		this.nome = nome;
+	}
+	
+	
 }
