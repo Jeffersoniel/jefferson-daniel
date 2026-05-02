8bd6a88a49bad22a13c8c0e4da0b607c2e943123
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-05-01 21:45:05 -0300

    adiciona

--- Escola/src/Main.java
+++ Escola/src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 	
 	public static void main(String[]args) {
 		
-		ListaDeAlunos sistema = new ListaDeAlunos() ();
+		ListaDeAlunos sistema = new ListaDeAlunos ();
 		
 		AlunoObjeto a1 = new AlunoObjeto("Jefferson" , 20 , 6 ,"1");
 		AlunoObjeto a2 = new AlunoObjeto("Guilherme" , 20 , 7 ,"1");
 		AlunoObjeto a3 = new AlunoObjeto("Henrique" , 20 , 5 ,"1");
 		
-		sistema.atualizaAlunos(a1);
-		sistema.atualizaAlunos(a2);
-		sistema.atualizaAlunos(a3);
+		sistema.adicionar(a1);
+		sistema.adicionar(a2);
+		sistema.adicionar(a3);
 		
 		System.out.println(sistema.ObterListaDeAlunos("Jefferson"));
 	}
