63c43e82b3ac87d86f9675e6845584e79a154fd9
Jefferson Daniel Santos Cavalheiro <danijeffo@gmail.com>
2026-05-18 20:48:39 -0300

    adiciona

--- VendaDeCarrro/src/Garagem.java
+++ VendaDeCarrro/src/Garagem.java
@@ -2,34 +2,29 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Garagem {
 
-   
-    private List<CarroFinanciado> listaCarros;
+	private List<CarroFinanciado> listaCarros;
 
-   
-    public Garagem() {
-        listaCarros = new ArrayList<CarroFinanciado>();
-    }
+	public Garagem() {
+		listaCarros = new ArrayList<CarroFinanciado>();
+	}
 
-  
-    public void adicionarCarro(CarroFinanciado carro) {
-        listaCarros.add(carro);
-    }
+	public void adicionarCarro(CarroFinanciado carro) {
+		listaCarros.add(carro);
+	}
 
- 
-    public CarroFinanciado obterCarroMaisCaro() {
-        CarroFinanciado carroMaisCaro = null;
-        double maiorPreco = 0.0;
+	public CarroFinanciado obterCarroMaisCaro() {
+		CarroFinanciado carroMaisCaro = null;
+		double maiorPreco = 0.0;
 
-        for (CarroFinanciado c : listaCarros) {
-            if (c.getPrecoVista() > maiorPreco) {
-                maiorPreco = c.getPrecoVista();
-                carroMaisCaro = c;
-            }
-        }
+		for (CarroFinanciado c : listaCarros) {
+			if (c.getPrecoVista() > maiorPreco) {
+				maiorPreco = c.getPrecoVista();
+				carroMaisCaro = c;
+			}
+		}
 
-        return carroMaisCaro;
-    }
+		return carroMaisCaro;
+	}
 }
--- VendaDeCarrro/src/VendasFinanciada.java
+++ VendaDeCarrro/src/VendasFinanciada.java
@@ -1,20 +1,20 @@
 public class VendasFinanciada {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        CarroFinanciado c1 = new CarroFinanciado("Honda Bros 160", 18500.00);
-        CarroFinanciado c2 = new CarroFinanciado("Honda XRE 300", 29000.00);
-        CarroFinanciado c3 = new CarroFinanciado("Chevrolet Onix", 65000.00);
-        CarroFinanciado c4 = new CarroFinanciado("Fiat Uno", 12000.00);
+		CarroFinanciado c1 = new CarroFinanciado("Honda Bros 160", 18500.00);
+		CarroFinanciado c2 = new CarroFinanciado("Honda XRE 300", 29000.00);
+		CarroFinanciado c3 = new CarroFinanciado("Chevrolet Onix", 65000.00);
+		CarroFinanciado c4 = new CarroFinanciado("Fiat Uno", 12000.00);
 
-        Garagem garagem = new Garagem();
+		Garagem garagem = new Garagem();
 
-        garagem.adicionarCarro(c1);
-        garagem.adicionarCarro(c2);
-        garagem.adicionarCarro(c3);
-        garagem.adicionarCarro(c4);
+		garagem.adicionarCarro(c1);
+		garagem.adicionarCarro(c2);
+		garagem.adicionarCarro(c3);
+		garagem.adicionarCarro(c4);
 
-        System.out.println("--- Carro mais caro do estoque ---");
-        System.out.println(garagem.obterCarroMaisCaro());
-    }
+		System.out.println("--- Carro mais caro do estoque ---");
+		System.out.println(garagem.obterCarroMaisCaro());
+	}
 }
